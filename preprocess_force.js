b1e05ccd98f1484ed656efcbed59f16c3ed5a6d5
Daniel Frysinger <daniel@frysinger.net>
2012-08-27 05:40:06 -0400

    Updated key to show partner types and changed key hover action to show links and neighbors of highlighted nodes.  Updated the popup pane to show all neighbors of hovered or clicked node.

--- preprocess_force.js
+++ preprocess_force.js
@@ -6,9 +6,12 @@
           node_arr = [],
           country_map = {},
           country_array = [],
+          type_map = {},
+          type_array = [],
           link = [],
           i = 0,
-          j = 0;
+          j = 0,
+          k = 0;
 
       function makeMap(name, data){
         var node = map[name];
@@ -17,37 +20,47 @@
           node_arr[i] = node;
           i++;
         }
-        if(node.country == undefined && data.Country != undefined){
-          if(country_map[data.Country] == undefined){
-            country_map[data.Country] = {name: data.Country, key: j};
-            country_array[j] = country_map[data.Country];
-            j++;
-          }
-          node.country = data.Country;
-          node.country_code = country_map[data.Country].key;
-
-          if(data.DollarValue.charAt(0) == "$"){
+        if(data.Vertex1 !== undefined){
+          if(data.DollarValue !== undefined && data.DollarValue.charAt(0) == "$"){
             var re = new RegExp("[$,]", "g");
             node.dvalue = data.DollarValue.replace(re, "");
           }
-          node_arr[node.key] = node
+          if(node.country == undefined && data.Country != undefined){
+            if(country_map[data.Country] == undefined){
+              country_map[data.Country] = {name: data.Country, key: j};
+              country_array[j] = country_map[data.Country];
+              j++;
+            }
+            node.country = data.Country;
+            node.country_code = country_map[data.Country].key;
+          }
+          if(node.type == undefined && data.PartnerType != undefined){
+            if(type_map[data.PartnerType] == undefined){
+              type_map[data.PartnerType] = {name: data.PartnerType, key: k};
+              type_array[k] = type_map[data.PartnerType];
+              k++;
+            }
+            node.type = data.PartnerType;
+            node.type_code = type_map[data.PartnerType].key;
+          }
+          node_arr[node.key] = node;
         }
       }
 
       nodes.forEach(function(d) {
-        makeMap(d.source, {});
-        makeMap(d.target, d)
+        makeMap(d.Vertex1, {});
+        makeMap(d.Vertex2, d)
       });
 
       nodes.forEach(function(d) {
-        if (d.target.length && d.source.length) {
-          map[d.target].links++;
-          map[d.source].links++;
-          link.push({source: map[d.source].key, target: map[d.target].key});
+        if (d.Vertex2.length && d.Vertex1.length) {
+          map[d.Vertex2].links++;
+          map[d.Vertex1].links++;
+          link.push({source: map[d.Vertex1].key, target: map[d.Vertex2].key});
         }
       });
 
-      return {nodes: node_arr, links: link, countries: country_array};
+      return {nodes: node_arr, links: link, countries: country_array, types: type_array};
     }
 
   };
